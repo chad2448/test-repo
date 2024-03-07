2a3dbb629ec025a01e1aab0e6bef254d0188e1a9
chad <cduren32@gmail.com>
2024-03-07 18:49:25 -0500

    add

--- test.sh
+++ test.sh
@@ -13,6 +13,6 @@ cat ~/.aws/credentials
 #apt update
 #apt install -y curl awscli unzip
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
+tar xzf awscliv2.zip
 sh ./aws/install
 aws s3 ls
