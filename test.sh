362fac2d970281499c64da90f95acddb51a2980c
chad <cduren32@gmail.com>
2024-03-07 18:39:32 -0500

    add

--- test.sh
+++ test.sh
@@ -8,8 +8,10 @@ aws_access_key_id = $AWS_ACCESS_KEY
 aws_secret_access_key = $AWS_SECRET_ACCESS_KEY
 EOF
 
-apt-get update
-apt-get install -y curl awscli unzip
+cat ~/.aws/credentials
+
+apt update
+apt install -y curl awscli unzip
 #curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 #unzip awscliv2.zip
 #sh ./aws/install
