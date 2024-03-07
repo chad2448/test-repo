6c3c53262b0b16f0cb53205332b46f2b669a81ea
chad <cduren32@gmail.com>
2024-03-07 17:06:03 -0500

    test

--- test.sh
+++ test.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+mkdir ~/.aws
+
 cat <<EOF > ~/.aws/credentials
 [default]
 aws_access_key_id = $AWS_ACCESS_KEY
 aws_secret_access_key = $AWS_SECRET_ACCESS_KEY
 EOF
 
-apt install awscli
+yum install awscli
 aws s3 ls
