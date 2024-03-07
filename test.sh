50041519433538484d3e7fa8562992849c25f0eb
chad <cduren32@gmail.com>
2024-03-07 18:33:02 -0500

    add

--- test.sh
+++ test.sh
@@ -8,7 +8,8 @@ aws_access_key_id = $AWS_ACCESS_KEY
 aws_secret_access_key = $AWS_SECRET_ACCESS_KEY
 EOF
 
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
-sh ./aws/install
+apt install curl awscli unzip
+#curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+#unzip awscliv2.zip
+#sh ./aws/install
 aws s3 ls
