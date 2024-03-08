44b1ccbed93cf4f28445184e627d081d9324c7c6
chad <cduren32@gmail.com>
2024-03-08 17:13:19 -0500

    add

--- test-2.sh
+++ test-2.sh
@@ -3,3 +3,11 @@
 go run go-info.go
 go run cpu.go
 go run memory.go
+
+NUM_PROC=$(go run cpu.go | grep "Number" | cut -d ':' -f2)
+
+if [ $NUM_PROC != '2' ]; then
+echo "Your total processor number is $NUM_PROC"
+else
+echo "Your total processor number is not 2. It's $NUM_PROC"
+fi
