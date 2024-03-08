09ba06a485df427e300129354b9123717ca610f8
chad <cduren32@gmail.com>
2024-03-08 16:44:21 -0500

    add

--- cpu.go
+++ cpu.go
@@ -8,4 +8,9 @@ import (
 func main() {
 	fmt.Println("Number of CPUs:", runtime.NumCPU())
 	fmt.Println("GOMAXPROCS:", runtime.GOMAXPROCS(0))
+	
+	memStats := &runtime.MemStats{}
+	runtime.ReadMemStats(memStats)
+	fmt.Printf("Total memory: %v bytes\n", memStats.TotalAlloc)
+	fmt.Printf("Memory allocated to heap objects: %v bytes\n", memStats.HeapAlloc)
 }
